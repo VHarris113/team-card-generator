90431f9a05b4d2afe8ae88cce81844d2c6c94f8b
Veronica Harris <veronica.n.harris92@gmail.com>
2021-04-12 17:58:32 -0400

    working on index.js

--- index.js
+++ index.js
@@ -46,30 +46,12 @@ function runApp() {
     fs.writeToFile('index.html', teamContent, err =>
     err ? console.log(err) : console.log("Congratulations! Your team cards are now available!")
 )});
-}
 
       function buildTeam() {
         if (!fs.existsSync(OUTPUT_DIR)) {
           fs.mkdirSync(OUTPUT_DIR)
         }
-        fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+        fs.writeFileSync(outputPath, render(), "utf-8");
       }
     runApp();
-    
-
-//     In index.js: 
-// const path = require("path");
-// const fs = require("fs");
-// const OUTPUT_DIR = path.resolve(__dirname, "dist")
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
-// function runApp() {
-//   ...Inquirer prompt and the functions that will ask users about manager, intern, and engineer.
-//   function buildTeam() {
-//     // Create the output directory if the output path doesn't exist
-//     if (!fs.existsSync(OUTPUT_DIR)) {
-//       fs.mkdirSync(OUTPUT_DIR)
-//     }
-//     fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
-//   }
-// }
-// runApp();
+}
